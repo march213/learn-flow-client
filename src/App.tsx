946d473cb69f407d72bcb16e9002268932982924
march213 <jane.molodetskaya@gmail.com>
2022-07-02 19:31:48 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;
